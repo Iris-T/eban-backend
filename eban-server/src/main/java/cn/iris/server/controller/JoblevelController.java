936b02cc602251c237e693421b027531940c861d
Iris-T <https://github.com/Iris-T>
2022-01-25 10:59:18 +0800

    职位、职称功能完成完善

--- eban-server/src/main/java/cn/iris/server/controller/JoblevelController.java
+++ eban-server/src/main/java/cn/iris/server/controller/JoblevelController.java
@@ -1,9 +1,16 @@
 package cn.iris.server.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import cn.iris.server.pojo.Joblevel;
+import cn.iris.server.pojo.RespBean;
+import cn.iris.server.service.IJoblevelService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * <p>
@@ -14,7 +21,52 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-01-19
  */
 @RestController
-@RequestMapping("/joblevel")
+@RequestMapping("/system/basic/joblevel")
 public class JoblevelController {
 
+    @Autowired
+    private IJoblevelService joblevelService;
+
+    @ApiOperation(value = "获取所有职称")
+    @GetMapping("/")
+    public List<Joblevel> getAllJoblevels() {
+        return joblevelService.list();
+    }
+
+    @ApiOperation(value = "添加职称")
+    @PostMapping("/")
+    public RespBean addJoblevel(@RequestBody Joblevel joblevel) {
+        joblevel.setCreateDate(LocalDateTime.now());
+        if (joblevelService.save(joblevel)) {
+            return RespBean.success("添加成功!");
+        }
+        return RespBean.error("添加失败!");
+    }
+
+    @ApiOperation(value = "更新职称")
+    @PutMapping("/")
+    public RespBean updateJoblevel(@RequestBody Joblevel joblevel) {
+        if (joblevelService.updateById(joblevel)) {
+            return RespBean.success("更新成功!");
+        }
+        return RespBean.error("更新失败!");
+    }
+
+    @ApiOperation(value = "删除职称")
+    @DeleteMapping("/{id}")
+    public RespBean delJoblevel(@PathVariable("id") Integer id) {
+        if (joblevelService.removeById(id)) {
+            return RespBean.success("删除成功!");
+        }
+        return RespBean.error("删除失败!");
+    }
+
+    @ApiOperation(value = "批量删除职称")
+    @DeleteMapping("/")
+    public RespBean delJoblevelByIds(Integer[] ids) {
+        if (joblevelService.removeByIds(Arrays.asList(ids))) {
+            return RespBean.success("删除成功!");
+        }
+        return RespBean.error("删除失败!");
+    }
 }
