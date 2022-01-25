7264fb4f4832bb17dfd4aea38e2b9202cbf5ad9b
Iris-T <https://github.com/Iris-T>
2022-01-25 10:21:07 +0800

    职位CURD功能完成

--- eban-server/src/main/java/cn/iris/server/controller/PositionController.java
+++ eban-server/src/main/java/cn/iris/server/controller/PositionController.java
@@ -1,9 +1,16 @@
 package cn.iris.server.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import cn.iris.server.pojo.Position;
+import cn.iris.server.pojo.RespBean;
+import cn.iris.server.service.IPositionService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * <p>
@@ -14,7 +21,53 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-01-19
  */
 @RestController
-@RequestMapping("/position")
+@RequestMapping("/system/config/pos")
 public class PositionController {
+
+    @Autowired
+    private IPositionService positionService;
+
+    @ApiOperation(value = "获取所有职位信息")
+    @GetMapping("/")
+    public List<Position> getAllPositions() {
+        return positionService.list();
+    }
+
+    @ApiOperation(value = "添加职位信息")
+    @PostMapping("/")
+    public RespBean addPosition(@RequestBody Position pos) {
+        pos.setCreateDate(LocalDateTime.now());
+        if (positionService.save(pos)) {
+            return RespBean.success("添加成功!");
+        }
+        return RespBean.error("添加失败!");
+    }
+
+    @ApiOperation(value = "更新职位信息")
+    @PutMapping("/")
+    public RespBean updatePosition(@RequestBody Position pos) {
+        if (positionService.updateById(pos)) {
+            return RespBean.success("更新成功!");
+        }
+        return RespBean.error("更新失败!");
+    }
+
+    @ApiOperation(value = "删除职位信息")
+    @DeleteMapping("/{id}")
+    public RespBean delPosition(@PathVariable Integer id) {
+        if (positionService.removeById(id)) {
+            return RespBean.success("删除成功!");
+        }
+        return RespBean.error("删除失败!");
+    }
+
+    @ApiOperation(value = "批量删除职位信息")
+    @DeleteMapping("/")
+    public RespBean delPositionByIds(Integer[] ids) {
+        if (positionService.removeByIds(Arrays.asList(ids))) {
+            return RespBean.success("删除成功!");
+        }
+        return RespBean.error("删除失败!");
+    }
 
 }
